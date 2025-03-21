1b8a102af0d9024a5ad8593cf03e8404db93f4f7
ainsley <ainsleyb2007@gmail.com>
2025-03-21 13:03:02 -0500

    Completed Assigment

--- program_2.py
+++ program_2.py
@@ -1,3 +1,8 @@
+#Program Written By: Ainsley Bellamy
+#Date Written: 03/21/2025
+#Program Title: Word_Separator
+
+
 # Program #2: Word Separator
 # Write a program that accepts as input a sentence in which all of the words are run together, 
 # but the first character of each word is uppercase.  
@@ -8,16 +13,28 @@
 # Start your changes on line 13
 
 def word_separator(sentence):
-
     new_sentence = ""
-    #    Add your logic here
+#First, strip sentence to remove unnecessary white space.
+    sentence = sentence.strip()
+#First add the first character of the string the user inputted, making sure it is uppercase.
+    new_sentence += sentence[0].upper()
+#Next, loop through all character except the first to check for uppercase letters.
+    for char in sentence[1:len(sentence)]:
+#If a letter is uppercase, first add a space to the new_sentence variable then
+# add that character in lowercase.
+        if char.isupper():
+            new_sentence += ' '
+        new_sentence += char.lower()
+#Finally, add a period cause it looks nice.
+    if sentence[-1] != '.':
+        new_sentence += '.'
+    return new_sentence
 
-    return new_sentence.strip()
+#Main function gets user input, runs above function, and prints results.
+def main():
+    sentence = str(input("Enter a sentence in which all the words to strung together and capitalized: "))
+    new_sentence = word_separator(sentence)
+    print(new_sentence)
 
-# Example usage
-
-sentence = "StopAndSmellTheRoses"
-
-new_sentence = word_separator(sentence)
-
-print(new_sentence)
+if __name__ == "__main__":
+    main()
